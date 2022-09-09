ed52c079fd2f5c11bd0cef0be2dd3ef3a8363d7c
Abhimanyu Debata <107947041+abhimanyu6542@users.noreply.github.com>
2022-09-09 22:22:51 +0530

    Create Weather.js

---
+++ src/components/Weather.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+const apiKey = "b7f172124e075c9c4c942b337794bef2";
+const Weather = () => {
+  const [longitude, setLongitude] = useState();
+  const [latitude, setLatitue] = useState();
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      const lol = navigator.geolocation.getCurrentPosition((pos) => {
+        setLatitue(pos.coords.longitude);
+        setLongitude(pos.coords.longitude);
+      });
+    }
+  };
+  const fetchNews = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,dail&appid=${apiKey}`
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+  useEffect(() => {
+    fetchNews();
+  }, []);
+  return (
+    <div>
+      <button onClick={getLocation}>lol</button>
+    </div>
+  );
+};
+
+export default Weather;
